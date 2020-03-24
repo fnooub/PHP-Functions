9aeb0c1ab14a927325ccaff25a988893fbb7af08
fnooub <61742373+fnooub@users.noreply.github.com>
2020-03-24 10:42:25 +0700

    them mang vao trong mang

---
+++ array_splice_assoc.php
@@ -0,0 +1,20 @@
+<?php
+
+$oldArray = array(
+    "color" => "red",
+    "taste" => "sweet",
+    "season" => "summer"
+);
+
+# Insert at offset 2
+$offset = 2;
+$newArray = array_slice($oldArray, 0, $offset, true) +
+            array('texture' => 'bumpy') +
+            array_slice($oldArray, $offset, NULL, true);
+
+print_r($newArray);
+print_r(array_insert($oldArray, array('texture' => array(1,2,3,4)), 2));
+
+function array_insert($array,$values,$offset) {
+    return array_slice($array, 0, $offset, true) + $values + array_slice($array, $offset, NULL, true);  
+}
